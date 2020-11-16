562c451dd220fdab97b9d7988b769a9303feeecc
badconfig <array.clean@gmail.com>
2020-11-17 01:32:16 +0300

    commit

--- static/js/auth.js
+++ static/js/auth.js
@@ -2,8 +2,8 @@ let signInButton = document.getElementById('sign-in');
 let signUpButton = document.getElementById('sign-up');
 let signInForm = document.getElementsByClassName('authorizer__form_sign-in')[0];
 let signUpForm = document.getElementsByClassName('authorizer__form_sign-up')[0];
-// signInButton.addEventListener('change', ChangeAuthType);
-// signUpButton.addEventListener('change', ChangeAuthType);
+signInButton.addEventListener('change', ChangeAuthType);
+signUpButton.addEventListener('change', ChangeAuthType);
 
 function ChangeAuthType() {
     signInForm.classList.toggle('authorizer__form_visible');
@@ -12,8 +12,10 @@ function ChangeAuthType() {
 
 let authLayout = document.getElementsByClassName('authorizer__background')[0];
 let authButton = document.getElementById('auth-button');
-// authButton.addEventListener('click', AuthToggle);
-// authLayout.addEventListener('click', AuthToggle);
+if (authButton != null) {
+    authButton.addEventListener('click', AuthToggle);
+    authLayout.addEventListener('click', AuthToggle);
+}
 
 function AuthToggle(evt) {
     if (evt.target === authLayout) {
