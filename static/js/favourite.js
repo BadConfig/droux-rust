32f0e1420deffd84769bb75778e7760aa770eea3
root <root@DESKTOP-K2PJR9Q.localdomain>
2020-11-17 04:33:29 +0300

    Реализован, но не оттестирован скрипт с добавлением в избранное.

--- static/js/favourite.js
+++ static/js/favourite.js
@@ -6,14 +6,17 @@ function addDelToFav(evt) {
     let wasInFav = 0;
     let icons = evt.currentTarget.querySelectorAll('img');
     console.log(icons);
-    for (let i = 0; i < icons.length; i++) {
-        icons[i].classList.toggle('fav-icon_active');
-    }
-        if (icons[0].classList.contains('fav-icon_active')) {
-        wasInFav = 1;
-    }
     let adId = icons[0].parentNode.parentNode.parentNode.getElementsByClassName('prod_id')[0].value;
     console.log(adId);
+    let same = document.querySelectorAll('input[value=' + adId+ ']');
+    for (let i = 0; i < same.length; i++) {
+        let sameIcons = same[i].querySelectorAll('img');
+        sameIcons[0].classList.toggle('fav-icon_active');
+        sameIcons[1].classList.toggle('fav-icon_active');
+    }
+    if (icons[0].classList.contains('fav-icon_active')) {
+        wasInFav = 1;
+    }
     let addToFav = new XMLHttpRequest();
     if (wasInFav === 0) {
         addToFav.open('POST', '/product/favourites/add', true);
