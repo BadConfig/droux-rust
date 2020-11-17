a3f5257ddcbf73c5dad5955721b708602e4990aa
root <root@DESKTOP-K2PJR9Q.localdomain>
2020-11-17 03:11:44 +0300

    Реализован, но не оттестирован скрипт с добавлением в избранное. э

--- static/js/favourite.js
+++ static/js/favourite.js
@@ -1,1 +1,25 @@
-let
+let favButtons = document.getElementsByClassName('ad__favourite-icon');
+for (let i=0; i < favButtons.length; i++) {
+    favButtons[i].addEventListener('click', addDelToFav);
+}
+function addDelToFav(evt) {
+    let wasInFav = 0;
+    let icons = evt.currentTarget.querySelectorAll('img');
+    console.log(icons);
+    for (let i = 0; i < icons.length; i++) {
+        icons[i].classList.toggle('fav-icon_active');
+    }
+        if (icons[0].classList.contains('fav-icon_active')) {
+        wasInFav = 1;
+    }
+    let adId = String(icons[0].parentNode.parentNode.getElementsByClassName('prod_id').value);
+    let addToFav = new XMLHttpRequest();
+    if (wasInFav === 0) {
+        addToFav.open('POST', '/product/favourites/add', true);
+    } else {
+        addToFav.open('POST', '/product/favourites/add/delete', true);
+    }
+    let body = 'prod_id=' + encodeURIComponent(adId);
+    addToFav.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    addToFav.send(body);
+}
