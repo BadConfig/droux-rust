79393099e1e71a9a6b560c57b7e771e6999e3e35
root <root@DESKTOP-K2PJR9Q.localdomain>
2020-11-26 11:28:52 +0300

    Логин

--- static/js/auth.js
+++ static/js/auth.js
@@ -50,7 +50,7 @@ function actionsShowHide(evt) {
 let alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 let alphabetHigh = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 let other = ['-', '$', '&', '%', '#', '@', '*']
-let numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 let login = document.getElementById('sign-up__login');
 
