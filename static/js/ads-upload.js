5e357f415350c9f02788deb285c852c0478cb6f6
root <root@DESKTOP-K2PJR9Q.localdomain>
2020-11-30 23:42:13 +0300

    filtersmustwork

--- static/js/ads-upload.js
+++ static/js/ads-upload.js
@@ -55,7 +55,7 @@ function NewSearch() {
     if (timeout != 0) {
         clearTimeout(timeout);
     }
-    timer = setInterval(checkAndAdd,3000);
+    // timer = setInterval(checkAndAdd,3000);
     timeout = setTimeout(useFilters, 1000);
 }
 
