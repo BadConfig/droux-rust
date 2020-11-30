a62a477ac82bf5f10c968adc9158938b4bdb5a37
root <root@DESKTOP-K2PJR9Q.localdomain>
2020-11-30 23:28:07 +0300

    filtersmustwork

--- static/js/ads-upload.js
+++ static/js/ads-upload.js
@@ -103,10 +103,10 @@ function useFilters() {
     main.append(searchResults);
     request.send(body);
     portions += 1;
-    // request.onreadystatechange = function() {
-    //     jsonToAds(request.response);
-    //     changeSize();
-    // }
+    request.onreadystatechange = function() {
+        jsonToAds(request.response);
+        changeSize();
+    }
     timeout = 0;
 
 }
