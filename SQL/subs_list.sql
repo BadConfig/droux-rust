fef43913d68ddc4abd250cd857c4d282ed23753f
badconfig <array.clean@gmail.com>
2020-12-18 05:32:06 +0300

    klj

--- SQL/subs_list.sql
+++ SQL/subs_list.sql
@@ -4,14 +4,14 @@ SELECT 	s.id,
 		EXISTS (
 			SELECT * 
 			FROM subscribes AS sub
-			WHERE sub.to_id = u2.id 
-				AND sub.from_id = u.id ) AS in_subs,
+			WHERE sub.from_id = u2.id 
+				AND sub.to_id = u.id ) AS in_subs,
 		u.picture_path
 FROM subscribes AS s 
 	JOIN users AS u
-		ON u.id = s.to_id
+		ON u.id = s.from_id
 	JOIN users AS u2 
-		ON u2.id = s.from_id
+		ON u2.id = s.to_id
 WHERE
 	s.to_id = $1
 LIMIT $2
