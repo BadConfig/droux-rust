f2efd9b0aadff4c2dd2cef8b6c27eb1a989d8aa1
badconfig <array.clean@gmail.com>
2020-12-19 06:35:37 +0300

    cron added

---
+++ cron_dev/run.sh
@@ -0,0 +1,1 @@
+psql postgres://docker:docker@postgres1_dev:5432/diesel_db -c "UPDATE products SET status = 'deleted' WHERE (EXTRACT(EPOCH FROM current_timestamp-create_datetime)/(60*60*60) > 60) AND status = 'published'; DELETE FROM views;"
