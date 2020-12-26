80e9a22ef1dab530a22c8ea86f8101bde55e8f08
root <root@DESKTOP-K2PJR9Q.localdomain>
2020-12-27 01:53:16 +0300

    Всё?

---
+++ static/js/news-carousel.js
@@ -0,0 +1,21 @@
+const newsArrowLeft = document.getElementsByClassName('carousel__arrow-div_position_left')[0];
+const newsArrowRight = document.getElementsByClassName('carousel__arrow-div_position_right')[0];
+const newsMarkers = document.getElementsByClassName('carousel__marker');
+const slider = document.getElementsByClassName('carousel__all-news')[0];
+
+let currentBanner = 0
+
+newsArrowLeft.addEventListener('click', SwipeNews);
+newsArrowRight.addEventListener('click', SwipeNews);
+
+function SwipeNews(e) {
+    let sliderWidth = slider.clientWidth;
+    newsMarkers[currentBanner].classList.remove('carousel__marker_active');
+    if ((e.target === newsArrowLeft) && (currentBanner > 0)) {
+        currentBanner -= 1;
+    } else if ((e.target === newsArrowRight) && (currentBanner > newsMarkers.length - 2)) {
+        currentBanner += 1;
+    }
+    newsMarkers[currentBanner].classList.add('carousel__marker_active');
+    slider.style.transform = "translateX(-" + (currentBanner * sliderWidth) + "px)";
+}
