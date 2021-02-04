f07086d114e2d61e64b26cf8582d4a1cb1466c64
root <root@DESKTOP-K2PJR9Q.localdomain>
2021-02-04 21:25:03 +0300

    script

--- static/js/hide-subcat.js
+++ static/js/hide-subcat.js
@@ -1,22 +1,18 @@
 const hide_categories_div = document.getElementsByClassName('filters__sector_category')[0];
-const hide_sub_categories_div = document.getElementsByClassName('filters__sector_type')[0];
-
 const hide_categories = hide_categories_div.getElementsByTagName('input');
-const hide_sub_categories = hide_sub_categories_div.getElementsByTagName('input');
 
-hide_categories.forEach((category) => {
-    category.addEventListener('change', (e) => {
+for (let i =0; i < hide_categories.length; i++) {
+    hide_categories[i].addEventListener('change', (e) => {
         if (document.getElementById('js_stylesheet') === null) {
             let styleSheet = document.createElement('style');
             styleSheet.id = "js_stylesheet";
             document.body.append(styleSheet);
         }
         document.getElementById('js_stylesheet').innerHTML = `
-            input[name="sub_category_id"]:not(input[data-parent="${e.target.value}"]) + label {
+            input[name="sub_category_id"]:not(input[data-parent="${e.currentTarget.value}"]) + label {
                 display: none;
             }
         `;
-
     })
-})
+}
 
