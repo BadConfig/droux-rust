157fa48ecf0058ca85f33baae4c81ca90c1a34cd
root <root@DESKTOP-K2PJR9Q.localdomain>
2021-02-22 02:06:16 +0300

    Вроде все без доп рутов

--- static/js/mobile-profile.js
+++ static/js/mobile-profile.js
@@ -9,6 +9,10 @@ function goToProfile() {
     card.classList.add('card_mob_vis');
 }
 
-if (window.location.href.includes('profile/users') && (window.location.href.includes('reviews') === false) && (window.location.href.includes('product') === false)) {
+if ((window.location.href.includes('profile/users') && (window.location.href.includes('reviews') === false
+    && window.location.href.includes('product') === false)) || ((window.location.href.includes('users/favourites') ||
+    window.location.href.includes('users/menu') || window.location.href.includes('users/products')) && (document.referrer.includes('users/favourites') ||
+    document.referrer.includes('users/menu') || document.referrer.includes('users/products')) === false)) {
     goToProfile();
 }
+
