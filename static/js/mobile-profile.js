92d7f68f713d6fba2709cb250af5eba9c88a19eb
root <root@DESKTOP-K2PJR9Q.localdomain>
2021-02-22 02:10:03 +0300

    Вроде все

--- static/js/mobile-profile.js
+++ static/js/mobile-profile.js
@@ -10,9 +10,8 @@ function goToProfile() {
 }
 
 if ((window.location.href.includes('profile/users') && (window.location.href.includes('reviews') === false
-    && window.location.href.includes('product') === false)) || ((window.location.href.includes('users/favourites') ||
-    window.location.href.includes('users/menu') || window.location.href.includes('users/products')) && (document.referrer.includes('users/favourites') ||
-    document.referrer.includes('users/menu') || document.referrer.includes('users/products')) === false)) {
+    && window.location.href.includes('product') === false)) || ((window.location.href.includes('users/favourites') || window.location.href.includes('users/products')) && (document.referrer.includes('users/favourites') ||
+    document.referrer.includes('users/menu') || document.referrer.includes('users/products')) === false) || window.location.href.includes('users/menu')) {
     goToProfile();
 }
 
